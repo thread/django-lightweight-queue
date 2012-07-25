9f26f12578e7f7905a95d40ec35ce7f77cc81a2b
Chris Lamb <chris@thread.com>
2012-07-24 18:47:38 -0700

    Add option for later.
    Signed-off-by: Chris Lamb <chris@thread.com>

--- django_lightweight_queue/management/commands/queue_runner.py
+++ django_lightweight_queue/management/commands/queue_runner.py
@@ -1,5 +1,12 @@
+from optparse import make_option
+
 from django.core.management.base import NoArgsCommand
 
 class Command(NoArgsCommand):
+    option_list = NoArgsCommand.option_list + (
+        make_option('--pidfile', action='store', dest='pidfile', default=None,
+            help="Fork and write pidfile to this file."),
+    )
+
     def handle_noargs(self, **options):
         pass
