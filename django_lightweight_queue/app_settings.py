ee6337d48e54a689b9c3600caf4d4491ae4fcb48
Chris Lamb <chris@thread.com>
2012-07-25 22:16:38 -0700

    Correct setting prefix.
    Signed-off-by: Chris Lamb <chris@thread.com>

--- django_lightweight_queue/app_settings.py
+++ django_lightweight_queue/app_settings.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
 def setting(suffix, default):
-    return getattr(settings, 'SIMPLE_QUEUE_%s' % suffix, default)
+    return getattr(settings, 'LIGHTWEIGHT_QUEUE_%s' % suffix, default)
 
 BACKEND = setting('BACKED', 'django_lightweight_queue.backends.synchronous.SynchronousBackend')
 MIDDLEWARE = setting('MIDDLEWARE', (
