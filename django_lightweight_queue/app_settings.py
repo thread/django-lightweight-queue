0cf45ad47d8b5e6e6df329990e76c4e5702f8e6c
Chris Lamb <chris@thread.com>
2012-07-25 22:17:12 -0700

    Correct typo in getting BACKEND.
    Signed-off-by: Chris Lamb <chris@thread.com>

--- django_lightweight_queue/app_settings.py
+++ django_lightweight_queue/app_settings.py
@@ -3,7 +3,7 @@ from django.conf import settings
 def setting(suffix, default):
     return getattr(settings, 'LIGHTWEIGHT_QUEUE_%s' % suffix, default)
 
-BACKEND = setting('BACKED', 'django_lightweight_queue.backends.synchronous.SynchronousBackend')
+BACKEND = setting('BACKEND', 'django_lightweight_queue.backends.synchronous.SynchronousBackend')
 MIDDLEWARE = setting('MIDDLEWARE', (
     'django_lightweight_queue.middleware.logging.LoggingMiddleware',
 ))
