b7d4f81ad78419b8dd4cd7c452a1095d7b25c5d7
Chris Lamb <chris@thread.com>
2012-07-25 22:29:25 -0700

    Use nicer logging.
    Signed-off-by: Chris Lamb <chris@thread.com>

--- django_lightweight_queue/management/commands/queue_runner.py
+++ django_lightweight_queue/management/commands/queue_runner.py
@@ -1,3 +1,5 @@
+import logging
+
 from optparse import make_option
 
 from django.core.management.base import NoArgsCommand
@@ -11,9 +13,24 @@ class Command(NoArgsCommand):
     )
 
     def handle_noargs(self, **options):
+        level = {
+            '0': logging.WARNING,
+            '1': logging.INFO,
+            '2': logging.DEBUG,
+        }[options['verbosity']]
+
+        logging.basicConfig(level=level, format='%(levelname).1s: %(message)s')
+        logging.info("Starting queue runner")
+
         backend = get_backend()
+        logging.info("Started backend %s", backend)
 
         while True:
-            job = backend.dequeue(1)
+            try:
+                logging.debug("Checking backend for items")
+                job = backend.dequeue(1)
+            except KeyboardInterrupt:
+                return
+
             if job is not None:
                 job.run()
