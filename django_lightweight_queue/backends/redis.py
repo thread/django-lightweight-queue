39dd51af16759ae5039fd526112b329e6f03c088
Chris Lamb <chris@thread.com>
2012-07-25 17:06:48 -0700

    Add Redis backend.
    Signed-off-by: Chris Lamb <chris@thread.com>

---
+++ django_lightweight_queue/backends/redis.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import # For 'redis'
+
+import redis
+
+from django.conf import settings
+from django.utils import simplejson
+
+from ..job import Job
+
+class RedisBackend(object):
+    KEY = 'django_lightweight_queue'
+
+    def __init__(self):
+        self.client = redis.Redis(
+            host=settings.REDIS_FEEDS_HOST,
+            port=settings.REDIS_FEEDS_PORT,
+        )
+
+    def enqueue(self, job):
+        self.client.rpush(self.KEY, job.to_json())
+
+    def dequeue(self, timeout):
+        data = self.redis.blpop(self.KEY, timeout)
+
+        if data is not None:
+            return Job.from_json(data)
