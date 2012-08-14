de144d7d32878642271a3087f35f20c3310874a4
Chris Lamb <chris@thread.com>
2012-08-13 17:14:12 -0700

    Need a named logger.
    Signed-off-by: Chris Lamb <chris@thread.com>

--- django_lightweight_queue/middleware/logging.py
+++ django_lightweight_queue/middleware/logging.py
@@ -3,19 +3,21 @@ from __future__ import absolute_import
 import logging
 import traceback
 
+log = logging.getLogger(__name__)
+
 class LoggingMiddleware(object):
     def process_job(self, job):
-        logging.info("Running job %s", job)
+        log.info("Running job %s", job)
 
     def process_result(self, job, result, duration):
-        logging.info("Finished job %s => %r (Time taken: %.2fs)",
+        log.info("Finished job %s => %r (Time taken: %.2fs)",
             job,
             result,
             duration,
         )
 
     def process_exception(self, job, duration, *exc_info):
-        logging.error("Exception when processing %r (duration: %.2fs): %s",
+        log.error("Exception when processing %r (duration: %.2fs): %s",
             job,
             duration,
             ''.join(traceback.format_exception(*exc_info)),
