5345cfddc24303a7d27b61865306775f93bb908c
Chris Lamb <chris@thread.com>
2013-05-08 11:27:44 +0100

    Add utility to import all submodules of all apps.
    Signed-off-by: Chris Lamb <chris@thread.com>

--- django_lightweight_queue/utils.py
+++ django_lightweight_queue/utils.py
@@ -1,6 +1,8 @@
-from django.utils.importlib import import_module
+from django.db.models import get_apps
 from django.core.exceptions import MiddlewareNotUsed
+from django.utils.importlib import import_module
 from django.utils.functional import memoize
+from django.utils.module_loading import module_has_submodule
 
 from . import app_settings
 
@@ -24,6 +26,21 @@ def get_middleware():
             pass
 
     return middleware
+
+def import_all_submodules(name):
+    for app_module in get_apps():
+        parts = app_module.__name__.split('.')
+        prefix, last = parts[:-1], parts[-1]
+
+        try:
+            import_module('.'.join(prefix + [name]))
+        except ImportError:
+            # Distinguise between tasks.py existing and failing to import
+            if last == 'models':
+                app_module = import_module('.'.join(prefix))
+
+            if module_has_submodule(app_module, name):
+                raise
 
 try:
     import setproctitle
