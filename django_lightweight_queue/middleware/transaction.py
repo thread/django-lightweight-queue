3e7d83d51fa43f8e93ad548b07193f13791f8abe
Chris Lamb <chris@thread.com>
2014-05-11 17:19:07 +0100

    Add a legacy version for older versions of Django.
    Signed-off-by: Chris Lamb <chris@thread.com>

--- django_lightweight_queue/middleware/transaction.py
+++ django_lightweight_queue/middleware/transaction.py
@@ -12,3 +12,22 @@ class TransactionMiddleware(object):
     def process_exception(self, job, time_taken, *exc_info):
         if not connection.in_atomic_block:
             transaction.rollback()
+
+# Legacy
+if not hasattr(connection, 'in_atomic_block'):
+    class TransactionMiddleware(object):
+        def process_job(self, job):
+            transaction.enter_transaction_management()
+            transaction.managed(True)
+
+        def process_result(self, job, result, duration):
+            if not transaction.is_managed():
+                return
+            if transaction.is_dirty():
+                transaction.commit()
+            transaction.leave_transaction_management()
+
+        def process_exception(self, job, time_taken, *exc_info):
+            if transaction.is_dirty():
+                transaction.rollback()
+            transaction.leave_transaction_management()
