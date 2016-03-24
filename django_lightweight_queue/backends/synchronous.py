9de1ce1def7915bf4587dbb0a4d9f396c77bc3b7
Christopher Baines <chris.b@thread.com>
2016-03-24 15:06:51 +0000

    Add length for the SynchronousBackend

--- django_lightweight_queue/backends/synchronous.py
+++ django_lightweight_queue/backends/synchronous.py
@@ -8,3 +8,8 @@ class SynchronousBackend(object):
         # Cannot dequeue from the synchronous backend but we can emulate by
         # never returning anything
         time.sleep(timeout)
+
+    def length(self, queue):
+        # The length is the number of items waiting to be processed, which can
+        # be defined as always 0 for the synchronous backend
+        return 0
