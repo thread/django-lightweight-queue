15e0d7f8d56c9d35a8eac14acc4c31a7faffdcda
Alistair Lynn <arplynn@gmail.com>
2018-06-21 16:25:19 +0100

    Remove legacy transaction handling

--- django_lightweight_queue/middleware/transaction.py
+++ django_lightweight_queue/middleware/transaction.py
@@ -1,4 +1,4 @@
-from django.db import transaction, connection
+from django.db import transaction
 
 class TransactionMiddleware(object):
     def process_job(self, job):
@@ -9,22 +9,3 @@ class TransactionMiddleware(object):
 
     def process_exception(self, job, time_taken, *exc_info):
         transaction.atomic().__exit__(*exc_info)
-
-# Legacy
-if not hasattr(connection, 'in_atomic_block'):
-    class TransactionMiddleware(object):
-        def process_job(self, job):
-            transaction.enter_transaction_management()
-            transaction.managed(True)
-
-        def process_result(self, job, result, duration):
-            if not transaction.is_managed():
-                return
-            if transaction.is_dirty():
-                transaction.commit()
-            transaction.leave_transaction_management()
-
-        def process_exception(self, job, time_taken, *exc_info):
-            if transaction.is_dirty():
-                transaction.rollback()
-            transaction.leave_transaction_management()
