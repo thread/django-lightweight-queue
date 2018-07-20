ee3343c5c91bfa5002c32f0f8ec0169e1dcad021
Peter Law <PeterJCLaw@gmail.com>
2018-07-20 16:45:05 +0100

    Also expose the TaskWrapper class
    This makes it easier to create a generic test helper that a task
    has been enqueued.

--- django_lightweight_queue/__init__.py
+++ django_lightweight_queue/__init__.py
@@ -1,7 +1,8 @@
-from .task import task
+from .task import task, TaskWrapper
 
 default_app_config = 'django_lightweight_queue.apps.DjangoLightweightQueueConfig'
 
 __all__ = (
     'task',
+    'TaskWrapper',
 )
