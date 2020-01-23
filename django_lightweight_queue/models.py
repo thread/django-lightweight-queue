7d99849afcad0e1890c0547be9e23144de986e00
Dan Palmer <dan@thread.com>
2020-01-23 16:00:21 +0000

    Now unused

--- django_lightweight_queue/models.py
+++
@@ -1,8 +0,0 @@
-from .utils import load_all_tasks
-
-try:
-    load_all_tasks()
-except RuntimeError:
-    # Initialisation tasks in latest versions of Django get run in AppConfig
-    # (see apps.py).
-    pass
