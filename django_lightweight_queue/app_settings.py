bacd80dfa2f2e51a23311fba5984662071809579
Peter Law <PeterJCLaw@gmail.com>
2020-04-29 22:37:58 +0100

    Annotate the settings

--- django_lightweight_queue/app_settings.py
+++ django_lightweight_queue/app_settings.py
@@ -1,40 +1,51 @@
+from typing import Dict, Union, Mapping, TypeVar, Callable, Sequence
+
 from django.conf import settings
 
 from . import constants
+from .types import Logger
+
+T = TypeVar('T')
 
 
-def setting(suffix, default):
+def setting(suffix: str, default: T) -> T:
     attr_name = '{}{}'.format(constants.SETTING_NAME_PREFIX, suffix)
     return getattr(settings, attr_name, default)
 
 
-WORKERS = setting('WORKERS', {})
-BACKEND = setting('BACKEND', 'django_lightweight_queue.backends.synchronous.SynchronousBackend')
+WORKERS = setting('WORKERS', {})  # type: Dict[str, int]
+BACKEND = setting(
+    'BACKEND',
+    'django_lightweight_queue.backends.synchronous.SynchronousBackend',
+)  # type: str
 
-LOGGER_FACTORY = setting('LOGGER_FACTORY', 'logging.getLogger')
+LOGGER_FACTORY = setting(
+    'LOGGER_FACTORY',
+    'logging.getLogger',
+)  # type: Union[str, Callable[[str], Logger]]
 
 # Allow per-queue overrides of the backend.
-BACKEND_OVERRIDES = setting('BACKEND_OVERRIDES', {})
+BACKEND_OVERRIDES = setting('BACKEND_OVERRIDES', {})  # type: Mapping[str, str]
 
 MIDDLEWARE = setting('MIDDLEWARE', (
     'django_lightweight_queue.middleware.logging.LoggingMiddleware',
     'django_lightweight_queue.middleware.transaction.TransactionMiddleware',
-))
+))  # type: Sequence[str]
 
 # Apps to ignore when looking for tasks. Apps must be specified as the dotted
 # name used in `INSTALLED_APPS`. This is expected to be useful when you need to
 # have a file called `tasks.py` within an app, but don't want
 # django-lightweight-queue to import that file.
 # Note: this _doesn't_ prevent tasks being registered from these apps.
-IGNORE_APPS = setting('IGNORE_APPS', ())
+IGNORE_APPS = setting('IGNORE_APPS', ())  # type: Sequence[str]
 
 # Backend-specific settings
-REDIS_HOST = setting('REDIS_HOST', '127.0.0.1')
-REDIS_PORT = setting('REDIS_PORT', 6379)
-REDIS_PREFIX = setting('REDIS_PREFIX', '')
+REDIS_HOST = setting('REDIS_HOST', '127.0.0.1')  # type: str
+REDIS_PORT = setting('REDIS_PORT', 6379)  # type: int
+REDIS_PREFIX = setting('REDIS_PREFIX', '')  # type: str
 
-ENABLE_PROMETHEUS = setting('ENABLE_PROMETHEUS', False)
+ENABLE_PROMETHEUS = setting('ENABLE_PROMETHEUS', False)  # type: bool
 # Workers will export metrics on this port, and ports following it
-PROMETHEUS_START_PORT = setting('PROMETHEUS_START_PORT', 9300)
+PROMETHEUS_START_PORT = setting('PROMETHEUS_START_PORT', 9300)  # type: int
 
-ATOMIC_JOBS = setting('ATOMIC_JOBS', True)
+ATOMIC_JOBS = setting('ATOMIC_JOBS', True)  # type: bool
---
+++ django_lightweight_queue/types.py
@@ -0,0 +1,68 @@
+from types import TracebackType
+from typing import Any, Dict, Tuple, Union, Optional
+
+from typing_extensions import Protocol
+
+_SysExcInfoType = Union[
+    Tuple[type, BaseException, Optional[TracebackType]],
+    Tuple[None, None, None],
+]
+
+_ExcInfoType = Union[None, bool, _SysExcInfoType, BaseException]
+
+
+class Logger(Protocol):
+    def debug(
+        self,
+        msg: str,
+        *args: Any,
+        exc_info: _ExcInfoType = ...,
+        stack_info: bool = ...,
+        extra: Optional[Dict[str, Any]] = ...,
+        **kwargs: Any
+    ) -> None:
+        ...
+
+    def info(
+        self,
+        msg: str,
+        *args: Any,
+        exc_info: _ExcInfoType = ...,
+        stack_info: bool = ...,
+        extra: Optional[Dict[str, Any]] = ...,
+        **kwargs: Any
+    ) -> None:
+        ...
+
+    def warning(
+        self,
+        msg: str,
+        *args: Any,
+        exc_info: _ExcInfoType = ...,
+        stack_info: bool = ...,
+        extra: Optional[Dict[str, Any]] = ...,
+        **kwargs: Any
+    ) -> None:
+        ...
+
+    def error(
+        self,
+        msg: str,
+        *args: Any,
+        exc_info: _ExcInfoType = ...,
+        stack_info: bool = ...,
+        extra: Optional[Dict[str, Any]] = ...,
+        **kwargs: Any
+    ) -> None:
+        ...
+
+    def exception(
+        self,
+        msg: str,
+        *args: Any,
+        exc_info: _ExcInfoType = ...,
+        stack_info: bool = ...,
+        extra: Optional[Dict[str, Any]] = ...,
+        **kwargs: Any
+    ) -> None:
+        ...
