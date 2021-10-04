thread/django-lightweight-queue

commit b2a4f4fbd2df8fb1ad5427c12626c27a46d3b152

Avoid variable re-use

django_lightweight_queue/management/commands/queue_configuration.py
@@ -41,9 +41,9 @@ class Command(BaseCommand):
         print("")
         print("Cron configuration")
 
-        for x in get_cron_config():
+        for config in get_cron_config():
             print("")
-            for k in (
+            for key in (
                 'command',
                 'command_args',
                 'hours',
@@ -52,4 +52,4 @@ class Command(BaseCommand):
                 'timeout',
                 'sigkill_on_stop',
             ):
-                print("{:20s}: {}".format(k, x.get(k, '-')))
+                print("{:20s}: {}".format(key, config.get(k, '-')))

commit 1192cb849fb23dfd516d40f5b32d86d76c4e765b

Use the right variable in this loop
This was broken accidentally in b2a4f4fbd2df8fb1ad5427c12626c27a46d3b152.

django_lightweight_queue/management/commands/queue_configuration.py
@@ -54,4 +54,4 @@ class Command(BaseCommand):
                 'timeout',
                 'sigkill_on_stop',
             ):
-                print("{:20s}: {}".format(key, config.get(k, '-')))
+                print("{:20s}: {}".format(key, config.get(key, '-')))