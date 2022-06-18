b8e22d37ed97f12a44291b3b8eeec8c7f3686a8e
Joe Kaufeld <opensource@joekaufeld.com>
2022-06-18 18:53:21 -0400

    fix django 4 regression

--- django_lightweight_queue/urls.py
+++ django_lightweight_queue/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 app_name = 'django_lightweight_queue'
 
 urlpatterns = (
-    url(r'^debug/django-lightweight-queue/debug-run$', views.debug_run, name='debug-run'),
+    re_path(r'^debug/django-lightweight-queue/debug-run$', views.debug_run, name='debug-run'),
 )
