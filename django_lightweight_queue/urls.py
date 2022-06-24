b7d71629cdaff34cae717c216b47222e650d7efb
Joe Kaufeld <opensource@joekaufeld.com>
2022-06-24 19:10:33 -0400

    convert from re_path to path

--- django_lightweight_queue/urls.py
+++ django_lightweight_queue/urls.py
@@ -1,9 +1,9 @@
-from django.urls import re_path
+from django.urls import path
 
 from . import views
 
 app_name = 'django_lightweight_queue'
 
 urlpatterns = (
-    re_path(r'^debug/django-lightweight-queue/debug-run$', views.debug_run, name='debug-run'),
+    path(r'debug/django-lightweight-queue/debug-run', views.debug_run, name='debug-run'),
 )
