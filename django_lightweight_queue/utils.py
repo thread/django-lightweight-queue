c9c5b849b44b0fbe15edee8b4cf9c55bbc8f8f96
Joe Kaufeld <opensource@joekaufeld.com>
2022-07-10 17:32:32 -0400

    Refactor for layer approach

--- django_lightweight_queue/utils.py
+++ django_lightweight_queue/utils.py
@@ -23,7 +23,7 @@ from django.utils.module_loading import module_has_submodule
 
 from . import constants
 from .types import Logger, QueueName, WorkerNumber
-from .app_settings import app_settings
+from .app_settings import Settings, app_settings
 
 if TYPE_CHECKING:
     from .backends.base import BaseBackend
@@ -34,6 +34,11 @@ FIVE_SECONDS = datetime.timedelta(seconds=5)
 
 
 def load_extra_config(file_path: str) -> None:
+
+    class Overrides(Settings):
+        """Container for holding internally overridden settings."""
+        pass
+
     extra_settings = imp.load_source('extra_settings', file_path)
 
     def get_setting_names(module: object) -> Set[str]:
@@ -54,9 +59,14 @@ def load_extra_config(file_path: str) -> None:
         warnings.warn("Ignoring unexpected setting(s) '{}'.".format(unexpected_str))
 
     override_names = extra_names - unexpected_names
+
+    overrides = Overrides()
+
     for name in override_names:
         short_name = name[len(constants.SETTING_NAME_PREFIX):]
-        setattr(app_settings, short_name, getattr(extra_settings, name))
+        setattr(overrides, short_name, getattr(extra_settings, name))
+
+    app_settings.add_layer(overrides)
 
 
 @lru_cache()
--- tests/test_task.py
+++ tests/test_task.py
@@ -30,7 +30,7 @@ class TaskTests(unittest.TestCase):
             'django_lightweight_queue.utils._accepting_implied_queues',
             new=False,
         ), unittest.mock.patch.dict(
-            'django_lightweight_queue.app_settings.Settings.WORKERS',
+            'django_lightweight_queue.app_settings.Defaults.WORKERS',
             workers,
         ):
             yield
@@ -54,7 +54,7 @@ class TaskTests(unittest.TestCase):
             return get_path(path)
 
         patch = mock.patch(
-            'django_lightweight_queue.app_settings.Settings.BACKEND',
+            'django_lightweight_queue.app_settings.Defaults.BACKEND',
             new='test-backend',
         )
         patch.start()
